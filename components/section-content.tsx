54d19220776bfa0ee94ee99635b4d3452c7c4cbd
ishi <berugen1st@gmail.com>
2024-03-03 17:59:55 +0700

    feat: update fixed media in section content

--- components/section-content.tsx
+++ components/section-content.tsx
@@ -17,7 +17,7 @@ export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
     },
     {
       title: "Clockwork",
-      url: "",
+      url: "/assets/output_3761611179_1.jpg",
       cryptoPrice: 1,
       cryptoCurrency: "SOL",
       convertedPrice: 97,
@@ -25,7 +25,7 @@ export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
     },
     {
       title: "Abstraction",
-      url: "",
+      url: "/assets/output_3952756105_0.jpg",
       cryptoPrice: 1,
       cryptoCurrency: "SOL",
       convertedPrice: 97,
@@ -33,7 +33,7 @@ export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
     },
     {
       title: "Uniqueness",
-      url: "",
+      url: "/assets/6769c8aa-23bf-43c9-93c6-204744e427db.jpeg",
       cryptoPrice: 1,
       cryptoCurrency: "SOL",
       convertedPrice: 97,
@@ -41,7 +41,7 @@ export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
     },
     {
       title: "Empowered",
-      url: "",
+      url: "/assets/output_3761611179_1.jpg",
       cryptoPrice: 1,
       cryptoCurrency: "SOL",
       convertedPrice: 97,
