dd282919d5031137db08c99d1b9a5debdc5ca630
ishi <berugen1st@gmail.com>
2024-03-03 17:41:19 +0700

    feat: add Email icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -287,3 +287,21 @@ export const MdiCreationIcon: React.FC<IconSvgProps> = (props) => {
     </svg>
   );
 };
+
+export const IcBaselineEmailIcon: React.FC<IconSvgProps> = (props) => {
+  const { width, height = 40 } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="#a889ff"
+        d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z"
+      ></path>
+    </svg>
+  );
+};
