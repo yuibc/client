86b1261fe9a3cc9facd28edc6911596f6bf242e3
ishi <berugen1st@gmail.com>
2024-03-03 18:44:10 +0700

    feat: adjust vertical gap in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SectionContent } from "@/components/section-content";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center w-full">
+    <section className="flex flex-col items-center justify-center w-full gap-y-5">
       <SectionContent header="TOP Trending" icon={<GgTrendingIcon />} />
       <SectionContent header="TOP Creation" icon={<MdiCreationIcon />} />
       <FooterSubscription
