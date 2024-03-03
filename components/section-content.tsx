6b6d1fdbac2f679132e5543a68d6ec41bcfa913c
ishi <berugen1st@gmail.com>
2024-03-03 18:01:53 +0700

    feat: move fixed data in section content to config

--- components/section-content.tsx
+++ components/section-content.tsx
@@ -1,54 +1,9 @@
-"use client";
-
 import { ArtBlock } from "./art-block";
-import { useState } from "react";
-import { SectionProps, TArtBlock } from "@/types";
+import { SectionProps } from "@/types";
 import { Button, Link } from "@nextui-org/react";
+import { fixedArts } from "@/config/fixed-data";
 
 export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
-  const [arts, _] = useState<TArtBlock[]>([
-    {
-      title: "Horses for Courses",
-      url: "/assets/output_3761611179_0.jpg",
-      cryptoPrice: 1,
-      cryptoCurrency: "SOL",
-      convertedPrice: 97,
-      currency: "$",
-    },
-    {
-      title: "Clockwork",
-      url: "/assets/output_3761611179_1.jpg",
-      cryptoPrice: 1,
-      cryptoCurrency: "SOL",
-      convertedPrice: 97,
-      currency: "$",
-    },
-    {
-      title: "Abstraction",
-      url: "/assets/output_3952756105_0.jpg",
-      cryptoPrice: 1,
-      cryptoCurrency: "SOL",
-      convertedPrice: 97,
-      currency: "$",
-    },
-    {
-      title: "Uniqueness",
-      url: "/assets/6769c8aa-23bf-43c9-93c6-204744e427db.jpeg",
-      cryptoPrice: 1,
-      cryptoCurrency: "SOL",
-      convertedPrice: 97,
-      currency: "$",
-    },
-    {
-      title: "Empowered",
-      url: "/assets/output_3761611179_1.jpg",
-      cryptoPrice: 1,
-      cryptoCurrency: "SOL",
-      convertedPrice: 97,
-      currency: "$",
-    },
-  ]);
-
   return (
     <div className="flex flex-col gap-3 w-full mb-5">
       <div className="flex justify-start items-center gap-3">
@@ -61,7 +16,7 @@ export const SectionContent = ({ header, icon }: Partial<SectionProps>) => {
         <h2 className="text-2xl font-semibold">{header}</h2>
       </div>
       <div className="flex gap-5 w-full">
-        {arts.map((item, index) => (
+        {fixedArts.map((item, index) => (
           <ArtBlock
             key={index}
             title={item.title}
