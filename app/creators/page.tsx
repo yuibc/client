f80f62887febd81661296fe4bbc7b35a86864aee
ishi <berugen1st@gmail.com>
2024-03-11 10:29:05 +0700

    feat: make temporary creators page

---
+++ app/creators/page.tsx
@@ -0,0 +1,23 @@
+import { CreatorBlock } from "@/components/creator-block";
+import { FaSolidUsersIcon, MingcuteUserStarFillIcon } from "@/components/icons";
+import { SectionContent } from "@/components/section-content";
+
+export default function Creators() {
+  return (
+    <section>
+      <div className="flex flex-col items-center mb-6">
+        <span className="my-7 flex flex-row gap-2">
+          <MingcuteUserStarFillIcon size={60} />
+          <h1 className="text-5xl font-bold">Creators of the month</h1>
+        </span>
+        <div className="flex justify-center items-center gap-3">
+          <CreatorBlock displayName="@User_Name_123" followerCount={10000} />
+          <CreatorBlock displayName="@User_Name_123" followerCount={6000} />
+          <CreatorBlock displayName="@User_Name_123" followerCount={4000} />
+        </div>
+      </div>
+      <hr className="my-10 border-default-50" />
+      <SectionContent header="Creators" icon={<FaSolidUsersIcon />} />
+    </section>
+  );
+}
