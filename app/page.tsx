aec698d58a489cc735d67c6cb66382b6100b7330
ishi <berugen1st@gmail.com>
2024-03-18 10:37:38 +0700

    feat: discard biomejs and add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,49 @@
-import { ArtBlock } from "@/components/art-block";
-import { FooterSubscription } from "@/components/footer-subscription";
-import { GgTrendingIcon, MdiCreationIcon } from "@/components/icons";
-import { title, subtitle } from "@/components/primitives";
-import { SectionContent } from "@/components/section-content";
-import { fixedArts } from "@/config/fixed-data";
+import { ArtBlock } from '@/components/art-block';
+import { FooterSubscription } from '@/components/footer-subscription';
+import { GgTrendingIcon, MdiCreationIcon } from '@/components/icons';
+import { title, subtitle } from '@/components/primitives';
+import { SectionContent } from '@/components/section-content';
+import { fixedArts } from '@/config/fixed-data';
 
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center w-full gap-y-5">
-      <SectionContent
-        header="TOP Trending"
-        icon={<GgTrendingIcon />}
-        gridSize={5}
-      >
-        {fixedArts.map((item, index) => (
-          <ArtBlock
-            key={index}
-            title={item.title}
-            currency={item.currency}
-            cryptoCurrency={item.cryptoCurrency}
-            convertedPrice={item.convertedPrice}
-            cryptoPrice={item.cryptoPrice}
-            url={item.url}
-          />
-        ))}
-      </SectionContent>
-      <SectionContent
-        header="TOP Creation"
-        icon={<MdiCreationIcon />}
-        gridSize={5}
-      >
-        {fixedArts.map((item, index) => (
-          <ArtBlock
-            key={index}
-            title={item.title}
-            currency={item.currency}
-            cryptoCurrency={item.cryptoCurrency}
-            convertedPrice={item.convertedPrice}
-            cryptoPrice={item.cryptoPrice}
-            url={item.url}
-          />
-        ))}
-      </SectionContent>
-      <FooterSubscription
-        subscriptionTitle="Interested in this digital world?"
-        communityTitle="Join the community"
-      />
-    </section>
-  );
+    return (
+        <section className="flex flex-col items-center justify-center w-full gap-y-5">
+            <SectionContent
+                header="TOP Trending"
+                icon={<GgTrendingIcon />}
+                gridSize={5}>
+                {fixedArts.map((item, index) => (
+                    <ArtBlock
+                        key={index}
+                        title={item.title}
+                        currency={item.currency}
+                        cryptoCurrency={item.cryptoCurrency}
+                        convertedPrice={item.convertedPrice}
+                        cryptoPrice={item.cryptoPrice}
+                        url={item.url}
+                    />
+                ))}
+            </SectionContent>
+            <SectionContent
+                header="TOP Creation"
+                icon={<MdiCreationIcon />}
+                gridSize={5}>
+                {fixedArts.map((item, index) => (
+                    <ArtBlock
+                        key={index}
+                        title={item.title}
+                        currency={item.currency}
+                        cryptoCurrency={item.cryptoCurrency}
+                        convertedPrice={item.convertedPrice}
+                        cryptoPrice={item.cryptoPrice}
+                        url={item.url}
+                    />
+                ))}
+            </SectionContent>
+            <FooterSubscription
+                subscriptionTitle="Interested in this digital world?"
+                communityTitle="Join the community"
+            />
+        </section>
+    );
 }
--- components/section-content.tsx
+++ components/section-content.tsx
@@ -1,52 +1,52 @@
-import { SectionProps } from "@/types";
-import { Button, Link } from "@nextui-org/react";
-import { FluentFilter24FilledIcon } from "./icons";
-import clsx from "clsx";
+import { SectionProps } from '@/types';
+import { Button, Link } from '@nextui-org/react';
+import { FluentFilter24FilledIcon } from './icons';
+import clsx from 'clsx';
 
 export const SectionContent = ({
-  header,
-  icon,
-  children,
-  hasFilter,
-  gridSize,
-  onClick,
+    header,
+    icon,
+    children,
+    hasFilter,
+    gridSize,
+    onClick,
 }: Partial<SectionProps>) => {
-  return (
-    <div className="flex flex-col gap-3 w-full mb-5">
-      <div className="flex justify-between items-center gap-3">
-        <span className="flex items-center gap-3">
-          <Button
-            isIconOnly
-            color="default"
-            startContent={icon}
-            variant="faded"
-          />
-          <h2 className="text-2xl font-semibold">{header}</h2>
-        </span>
-        {hasFilter && (
-          <Button
-            color="default"
-            variant="flat"
-            startContent={<FluentFilter24FilledIcon />}
-            onClick={onClick}
-          >
-            Filter
-          </Button>
-        )}
-      </div>
-      <div
-        className={clsx(
-          "grid gap-5",
-          { "grid-cols-3": gridSize === 3 },
-          { "grid-cols-4": gridSize === 4 },
-          { "grid-cols-5": gridSize === 5 },
-        )}
-      >
-        {children}
-      </div>
-      <div className="flex justify-end">
-        <Link className="text-md font-semibold cursor-pointer">See more</Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-3 w-full mb-5">
+            <div className="flex justify-between items-center gap-3">
+                <span className="flex items-center gap-3">
+                    <Button
+                        isIconOnly
+                        color="default"
+                        startContent={icon}
+                        variant="faded"
+                    />
+                    <h2 className="text-2xl font-semibold">{header}</h2>
+                </span>
+                {hasFilter && (
+                    <Button
+                        color="default"
+                        variant="flat"
+                        startContent={<FluentFilter24FilledIcon />}
+                        onClick={onClick}>
+                        Filter
+                    </Button>
+                )}
+            </div>
+            <div
+                className={clsx(
+                    'grid gap-5',
+                    { 'grid-cols-3': gridSize === 3 },
+                    { 'grid-cols-4': gridSize === 4 },
+                    { 'grid-cols-5': gridSize === 5 },
+                )}>
+                {children}
+            </div>
+            <div className="flex justify-end">
+                <Link className="text-md font-semibold cursor-pointer">
+                    See more
+                </Link>
+            </div>
+        </div>
+    );
 };
