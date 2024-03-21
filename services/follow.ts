3b1bb5821de2c93a0566cd6d9204688e7f3f53b3
ishi <berugen1st@gmail.com>
2024-03-21 08:39:27 +0700

    feat: add follow service

---
+++ services/follow.ts
@@ -0,0 +1,32 @@
+export function useFollow(url = process.env.NEXT_PUBLIC_YUI_SERVER) {
+    const followers = async (userId: number) => {
+        try {
+            const res = await fetch(`${url}/user/${userId}/followers`, {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            });
+            return await res.json();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const follow = async (userId: number) => {
+        try {
+            const res = await fetch(`${url}/${userId}/follow`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({}),
+            });
+            console.log(await res.text());
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return { followers, follow };
+}
