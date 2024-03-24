7aa918641b0e572247ef0c01bc60b363f9a285a6
ishi <berugen1st@gmail.com>
2024-03-24 19:52:07 +0700

    feat: re-add wallet provider in providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,18 +5,19 @@ import { NextUIProvider } from '@nextui-org/system';
 import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
+import { WalletProvider } from '@solana/wallet-adapter-react';
 
 export interface ProvidersProps {
     children: React.ReactNode;
     themeProps?: ThemeProviderProps;
 }
-
 export function Providers({ children, themeProps }: ProvidersProps) {
     const router = useRouter();
-
     return (
         <NextUIProvider navigate={router.push}>
-            <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+            <NextThemesProvider {...themeProps}>
+                <WalletProvider wallets={[]}>{children}</WalletProvider>
+            </NextThemesProvider>
         </NextUIProvider>
     );
 }
