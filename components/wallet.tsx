fc81d85d9033bb81cc74b4fcc56374f06aa8d0f2
ishi <berugen1st@gmail.com>
2024-03-24 16:37:29 +0700

    feat: add wallet connection component

---
+++ components/wallet.tsx
@@ -0,0 +1,122 @@
+import React, { FC, useMemo, useState } from 'react';
+import {
+    ConnectionProvider,
+    useWallet,
+    Wallet as TWallet,
+} from '@solana/wallet-adapter-react';
+import {
+    WalletAdapterNetwork,
+    WalletReadyState,
+    Adapter,
+} from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import {
+    Button,
+    Link,
+    Listbox,
+    ListboxItem,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+} from '@nextui-org/react';
+import { WalletLoginIcon } from './icons';
+import { WalletIcon } from '@solana/wallet-adapter-react-ui';
+
+export const Wallet: FC = () => {
+    const [isOpen, setOpen] = useState(false);
+    const { wallets } = useWallet();
+
+    const network = WalletAdapterNetwork.Testnet;
+
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const [listedWallets, _] = useMemo(() => {
+        const installed: TWallet[] = [];
+        const notInstalled: TWallet[] = [];
+
+        for (const wallet of wallets) {
+            if (wallet.readyState === WalletReadyState.Installed) {
+                installed.push(wallet);
+            } else {
+                notInstalled.push(wallet);
+            }
+        }
+
+        return installed.length
+            ? [installed, notInstalled]
+            : [notInstalled, []];
+    }, [wallets]);
+
+    const connectWallet = async (adapter: Adapter) => {
+        await adapter.connect();
+        if (adapter.connected) {
+            localStorage.setItem(
+                'walletAddress',
+                adapter.publicKey?.toString() || '',
+            );
+            location.reload();
+        }
+    };
+
+    return (
+        <>
+            <ConnectionProvider endpoint={endpoint}>
+                <Button
+                    isExternal
+                    as={Link}
+                    startContent={<WalletLoginIcon />}
+                    onPress={() => setOpen(!isOpen)}
+                    variant="flat">
+                    Connect
+                </Button>
+                <Modal
+                    backdrop="blur"
+                    isOpen={isOpen}
+                    onClose={() => setOpen(false)}>
+                    <ModalContent>
+                        <ModalBody className="text-center mt-5 font-semibold">
+                            <h2 className="text-2xl">
+                                Connect a wallet on Solana to continue
+                            </h2>
+                        </ModalBody>
+                        <ModalFooter className="flex justify-center flex-col">
+                            <Listbox
+                                variant="faded"
+                                disallowEmptySelection
+                                aria-label="Wallet Selection"
+                                selectionMode="single">
+                                {listedWallets.map((wallet) => (
+                                    <ListboxItem
+                                        key={wallet.adapter.name}
+                                        textValue={wallet.adapter.name}
+                                        onClick={() =>
+                                            connectWallet(wallet.adapter)
+                                        }
+                                        startContent={
+                                            <WalletIcon
+                                                wallet={wallet}
+                                                width={36}
+                                            />
+                                        }>
+                                        <span className="flex justify-between items-center p-3">
+                                            <span className="font-semibold">
+                                                {wallet.adapter.name}
+                                            </span>
+                                            <span className="font-mono">
+                                                {wallet.readyState ===
+                                                WalletReadyState.Installed
+                                                    ? 'Detected'
+                                                    : ''}
+                                            </span>
+                                        </span>
+                                    </ListboxItem>
+                                ))}
+                            </Listbox>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </ConnectionProvider>
+        </>
+    );
+};
