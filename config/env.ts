b87f2b3ff158f7545a1b73e025f613c51397c4d0
ishi <berugen1st@gmail.com>
2024-03-25 16:44:38 +0700

    feat: isolate env

---
+++ config/env.ts
@@ -0,0 +1,3 @@
+export const SERVER_URL = process.env.NEXT_PUBLIC_YUI_SERVER;
+export const SERVER_API_ROUTE = process.env.NEXT_PUBLIC_YUI_API_ROUTE;
+export const BASE_URL = `${SERVER_URL}${SERVER_API_ROUTE}`;
