6c8ad607a42d6b536ed5f44afa3936bc7a3e0e5c
ishi <berugen1st@gmail.com>
2024-03-26 17:29:58 +0700

    feat: add cart and added items state

--- services/jotai.ts
+++ services/jotai.ts
@@ -1,3 +1,8 @@
+import { TArtwork } from '@/types';
 import { atom } from 'jotai';
 
 export const isAuthAtom = atom(false);
+
+export const shoppingCartAtom = atom<Partial<TArtwork>[]>([]);
+
+export const addedItemsAtom = atom<{ [K: string]: number }>({});
