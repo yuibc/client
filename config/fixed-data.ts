577491862b3137982b91670e666c9e1e367d274f
ishi <berugen1st@gmail.com>
2024-03-26 15:14:33 +0700

    feat: update fixed data

--- config/fixed-data.ts
+++ config/fixed-data.ts
@@ -1,44 +1,44 @@
 import { TArtBlock, TCreator } from '@/types';
 
-export const fixedArts: TArtBlock[] = [
+export const fixedArts: Omit<TArtBlock, 'isDashboardItem'>[] = [
     {
+        artworkId: 1,
         title: 'Horses for Courses',
         url: '/assets/output_3761611179_0.jpg',
         cryptoPrice: 1,
         cryptoCurrency: 'SOL',
-        convertedPrice: 97,
         currency: '$',
     },
     {
+        artworkId: 2,
         title: 'Clockwork',
         url: '/assets/output_3761611179_1.jpg',
-        cryptoPrice: 1,
+        cryptoPrice: 0.012,
         cryptoCurrency: 'SOL',
-        convertedPrice: 97,
         currency: '$',
     },
     {
+        artworkId: 3,
         title: 'Abstraction',
         url: '/assets/output_3952756105_0.jpg',
-        cryptoPrice: 1,
+        cryptoPrice: 0.0025,
         cryptoCurrency: 'SOL',
-        convertedPrice: 97,
         currency: '$',
     },
     {
+        artworkId: 4,
         title: 'Uniqueness',
         url: '/assets/6769c8aa-23bf-43c9-93c6-204744e427db.jpeg',
-        cryptoPrice: 1,
+        cryptoPrice: 0.001,
         cryptoCurrency: 'SOL',
-        convertedPrice: 97,
         currency: '$',
     },
     {
+        artworkId: 5,
         title: 'Empowered',
         url: '/assets/output_3761611179_1.jpg',
-        cryptoPrice: 1,
+        cryptoPrice: 0.01,
         cryptoCurrency: 'SOL',
-        convertedPrice: 97,
         currency: '$',
     },
 ];
