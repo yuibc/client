96ed035cd47aab81c6971145f972069c76dfb4ca
ishi <berugen1st@gmail.com>
2024-03-26 15:13:43 +0700

    feat: update metaplex service

--- services/metaplex.ts
+++ services/metaplex.ts
@@ -3,92 +3,78 @@ import {
     TokenStandard,
     createNft,
     createV1,
-    fetchDigitalAsset,
     mintV1,
     mplTokenMetadata,
 } from '@metaplex-foundation/mpl-token-metadata';
 import {
     KeypairSigner,
+    PublicKey,
+    Umi,
+    generateSigner,
     percentAmount,
     publicKey,
 } from '@metaplex-foundation/umi';
-import { NFTStorage } from 'nft.storage';
 
-type TNFT = {
+type TMintData = {
     name: string;
     uri: string;
+    walletAddress: string;
 };
 
-type TArtwork = {
-    title: string;
-    description: string;
-    owner: string;
-    artwork: File | Blob;
-};
-
-export function useMetaplex() {
+export function useMetaplexUmi() {
     const umi = createUmi('https://api.testnet.solana.com/').use(
         mplTokenMetadata(),
     );
 
-    const storage = new NFTStorage({
-        token: process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY as string,
-    });
+    const sellerFeeBasisPoints = percentAmount(2.0, 2);
 
-    const uploadArtwork = async ({
-        title,
-        description,
-        artwork,
-        owner,
-    }: TArtwork) => {
-        return await storage.store({
-            name: title,
-            description,
-            image: artwork,
-            owner,
-        });
-    };
+    const mint = (umi: Umi) => generateSigner(umi);
 
-    const fetchAsset = async (mint: KeypairSigner) =>
-        await fetchDigitalAsset(umi, mint.publicKey);
-
-    const createAccount = async (mint: KeypairSigner, { name, uri }: TNFT) =>
+    const createAccount = async (
+        umi: Umi,
+        mint: KeypairSigner,
+        { name, uri }: Omit<TMintData, 'walletAddress'>,
+    ) =>
         await createV1(umi, {
             mint,
-            authority: mint,
             name,
             uri,
-            sellerFeeBasisPoints: percentAmount(0.0),
+            sellerFeeBasisPoints,
             tokenStandard: TokenStandard.NonFungible,
         }).sendAndConfirm(umi);
 
-    const nft = async (
-        mint: KeypairSigner,
+    const mintToken = async (
+        umi: Umi,
+        pk: PublicKey,
+        authority: KeypairSigner,
         walletAddress: string,
-        { name, uri }: TNFT,
     ) =>
-        await createNft(umi, {
-            mint,
-            name,
-            uri,
-            sellerFeeBasisPoints: percentAmount(5.5),
-            tokenOwner: publicKey(walletAddress),
-        }).sendAndConfirm(umi);
-
-    const mintToken = async (mint: KeypairSigner, walletAddress: string) =>
         await mintV1(umi, {
-            mint: mint.publicKey,
+            mint: pk,
+            authority,
             amount: 1,
             tokenOwner: publicKey(walletAddress),
             tokenStandard: TokenStandard.NonFungible,
         }).sendAndConfirm(umi);
 
+    const nft = async (
+        umi: Umi,
+        mint: KeypairSigner,
+        { name, uri, walletAddress }: TMintData,
+    ) =>
+        await createNft(umi, {
+            mint,
+            name,
+            uri,
+            sellerFeeBasisPoints,
+            tokenOwner: publicKey(walletAddress),
+        }).sendAndConfirm(umi);
+
     return {
-        fetchAsset,
-        uploadArtwork,
+        umi,
+        mint,
+        nft,
         createAccount,
         mintToken,
-        umi,
-        nft,
     };
 }
