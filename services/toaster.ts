fa1bf0fc9bac4c33bff7acd3f350469f6b6b1d6e
ishi <berugen1st@gmail.com>
2024-03-31 22:12:27 +0700

    feat: add extra config for toaster

--- services/toaster.ts
+++ services/toaster.ts
@@ -1,16 +1,19 @@
 import { toast } from 'react-hot-toast';
 
-export function useToast(duration = 3000) {
+export function useToast(DURATION = 3000) {
     const config = {
         style: {
             borderRadius: '10px',
             background: '#333',
             color: '#fff',
+            padding: '16px',
         },
-        duration,
+        duration: DURATION,
     };
-    const onSuccess = (message: string) => toast.success(message, config);
-    const onError = (message: string) => toast.error(message, config);
+    const onSuccess = (message: string, duration?: number) =>
+        toast.success(message, { ...config, duration: duration || DURATION });
+    const onError = (message: string, duration?: number) =>
+        toast.error(message, { ...config, duration: duration || DURATION });
 
     return { onSuccess, onError };
 }
