ebe3ef1ad903ce5e17cc8fb37ca7c1feaf5ae373
ishi <berugen1st@gmail.com>
2024-04-02 19:20:32 +0700

    feat: add skeleton for artworks in home

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { fixedArts } from '@/config/fixed-data';
 import { toIPFSGateway } from '@/helpers';
 import { useArtwork } from '@/services';
 import { TArtwork } from '@/types';
+import { Card, Skeleton } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -52,6 +53,25 @@ export default function Home() {
                         isDashboardItem={isCreator(item.walletAddress)}
                     />
                 ))}
+                {presentArtworks.length === 0 &&
+                    [1, 2, 3, 4, 5].map((item) => (
+                        <Card key={item} className="p-4 space-y-5" radius="lg">
+                            <Skeleton className="rounded-lg">
+                                <div className="h-24 rounded-lg bg-default-300"></div>
+                            </Skeleton>
+                            <div className="space-y-3">
+                                <Skeleton className="w-3/5 rounded-lg">
+                                    <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                                </Skeleton>
+                                <Skeleton className="w-4/5 rounded-lg">
+                                    <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                                </Skeleton>
+                                <Skeleton className="w-2/5 rounded-lg">
+                                    <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+                                </Skeleton>
+                            </div>
+                        </Card>
+                    ))}
             </SectionContent>
             <SectionContent
                 limited
