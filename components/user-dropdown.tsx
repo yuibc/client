9c138ef94831e9a7945b73b574d73317d34020cc
ishi <berugen1st@gmail.com>
2024-04-03 13:30:35 +0700

    feat: remove reconnect, switch wallet button

--- components/user-dropdown.tsx
+++ components/user-dropdown.tsx
@@ -19,7 +19,11 @@ import { CreatorBlock } from './creator-block';
 import { useFollow } from '@/services';
 import { useEffect, useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { PhantomWalletName } from '@solana/wallet-adapter-wallets';
+import {
+    PhantomWalletAdapter,
+    PhantomWalletName,
+} from '@solana/wallet-adapter-wallets';
+import { Profile } from './profile';
 
 export const UserDropdown = ({
     displayName,
@@ -28,6 +32,7 @@ export const UserDropdown = ({
     const { followers } = useFollow();
     const [followerCount, setFollowerCount] = useState(0);
     const wallet = useWallet();
+    const [openProfile, setOpenProfile] = useState(false);
 
     const signOut = () => {
         localStorage.clear();
@@ -43,83 +48,69 @@ export const UserDropdown = ({
         setFollowerCount(fws.length);
     };
 
-    const reconnectWallet = async () => {
-        const walletName = localStorage.getItem('walletName');
-        if (!walletName) return;
-        // Default
-        if (walletName === PhantomWalletName) {
-            wallet.select(PhantomWalletName);
-            await wallet.connect();
-        }
-    };
-
     useEffect(() => {
         fetchFollowerCount();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
-        <Dropdown backdrop="opaque">
-            <DropdownTrigger>
-                <Button
-                    size="md"
-                    onClick={onClick}
-                    className="text-sm font-semibold text-default-600 bg-default-100"
-                    startContent={<WalletLoginIcon />}
-                    variant="flat">
-                    {displayName}
-                </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-                aria-label="Dynamic Actions"
-                disabledKeys={[
-                    wallet.connected ? 'reconnect' : '',
-                    'change-wallet',
-                ]}>
-                <DropdownItem>
-                    <CreatorBlock
-                        displayName={displayName}
-                        followerCount={followerCount}
-                        borderless
-                        noFollowButton
-                    />
-                </DropdownItem>
-                <DropdownItem
-                    key="profile"
-                    className="py-3"
-                    startContent={<IconamoonProfileCircleFill />}>
-                    Profile
-                </DropdownItem>
-                <DropdownItem
-                    key="reconnect"
-                    className="py-3"
-                    onPress={reconnectWallet}
-                    startContent={<FluentPlugConnected24Filled />}>
-                    Reconnect
-                </DropdownItem>
-                <DropdownItem
-                    key="change-wallet"
-                    className="py-3"
-                    onPress={reconnectWallet}
-                    startContent={<GgArrowsExchangeV />}>
-                    Change Wallet
-                </DropdownItem>
-                <DropdownItem
-                    key="dashboard"
-                    as={Link}
-                    href="/dashboard"
-                    className="py-3"
-                    startContent={<IcRoundDashboardIcon />}>
-                    Dashboard
-                </DropdownItem>
-                <DropdownItem
-                    key="sign-out"
-                    color="danger"
-                    className="text-danger py-3"
-                    onPress={signOut}
-                    startContent={<LetsIconsSignOutSqureFill />}>
-                    Sign out
-                </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
+        <>
+            <Profile
+                isOpen={openProfile}
+                onClose={() => setOpenProfile(false)}
+            />
+            <Dropdown backdrop="opaque">
+                <DropdownTrigger>
+                    <Button
+                        size="md"
+                        onClick={onClick}
+                        className="text-sm font-semibold text-default-600 bg-default-100"
+                        startContent={<WalletLoginIcon />}
+                        variant="flat">
+                        {displayName}
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                    aria-label="Dynamic Actions"
+                    disabledKeys={[
+                        wallet.connected ? 'reconnect' : '',
+                        'change-wallet',
+                    ]}>
+                    <DropdownItem>
+                        <CreatorBlock
+                            displayName={displayName}
+                            followerCount={followerCount}
+                            borderless
+                            noFollowButton
+                        />
+                    </DropdownItem>
+                    <DropdownItem
+                        key="profile"
+                        className="py-3"
+                        aria-label="profile"
+                        onPress={() => setOpenProfile(true)}
+                        startContent={<IconamoonProfileCircleFill />}>
+                        Profile
+                    </DropdownItem>
+                    <DropdownItem
+                        key="dashboard"
+                        as={Link}
+                        href="/dashboard"
+                        className="py-3"
+                        aria-label="dashboard"
+                        startContent={<IcRoundDashboardIcon />}>
+                        Dashboard
+                    </DropdownItem>
+                    <DropdownItem
+                        key="sign-out"
+                        color="danger"
+                        aria-label="signOut"
+                        className="text-danger py-3"
+                        onPress={signOut}
+                        startContent={<LetsIconsSignOutSqureFill />}>
+                        Sign out
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </>
     );
 };
